cde67c39586ab942e10ba085867b344322fe8f82
fckaw <feichin.kaw@gmail.com>
2025-11-30 17:54:20 +0000

    Initial upload

---
+++ puzzles.js
@@ -0,0 +1,150 @@
+// AUTO-GENERATED from puzzles.csv – do not edit by hand
+window.puzzles = [
+  {
+    "id": 1,
+    "mode": "phrase",
+    "answer": "head-to-head",
+    "clue": "Two bucks facing each other",
+    "image": "1.jpg"
+  },
+  {
+    "id": 2,
+    "mode": "phrase",
+    "answer": "lady of leisure",
+    "clue": "Stretching cat",
+    "image": "2.jpg"
+  },
+  {
+    "id": 3,
+    "mode": "phrase",
+    "answer": "nine lives",
+    "clue": "Three cats",
+    "image": "3.jpg"
+  },
+  {
+    "id": 4,
+    "mode": "phrase",
+    "answer": "slow traffic",
+    "clue": "Herd of sheep on the road",
+    "image": "4.mp4"
+  },
+  {
+    "id": 5,
+    "mode": "phrase",
+    "answer": "silence is the loudest sound",
+    "clue": "Circular, abandoned structure",
+    "image": "5.jpg"
+  },
+  {
+    "id": 6,
+    "mode": "phrase",
+    "answer": "rock and roll",
+    "clue": "Vehicle stuck in rocks",
+    "image": "6.mp4"
+  },
+  {
+    "id": 7,
+    "mode": "phrase",
+    "answer": "guided by the glow",
+    "clue": "Yellow moon on top of a church",
+    "image": "7.jpg"
+  },
+  {
+    "id": 8,
+    "mode": "phrase",
+    "answer": "felfoldi tabor",
+    "clue": "Equivalent in Hungarian?",
+    "image": "8.jpg"
+  },
+  {
+    "id": 9,
+    "mode": "phrase",
+    "answer": "roasted",
+    "clue": "",
+    "image": "9.jpg"
+  },
+  {
+    "id": 10,
+    "mode": "phrase",
+    "answer": "curiosity kills",
+    "clue": "",
+    "image": "10.jpg"
+  },
+  {
+    "id": 11,
+    "mode": "phrase",
+    "answer": "bend the rules",
+    "clue": "",
+    "image": "11.jpg"
+  },
+  {
+    "id": 12,
+    "mode": "phrase",
+    "answer": "feed me now!",
+    "clue": "",
+    "image": "12.jpg"
+  },
+  {
+    "id": 13,
+    "mode": "phrase",
+    "answer": "all that glitters is not gold",
+    "clue": "",
+    "image": "13.jpg"
+  },
+  {
+    "id": 14,
+    "mode": "phrase",
+    "answer": "fall into place",
+    "clue": "",
+    "image": "14.jpg"
+  },
+  {
+    "id": 15,
+    "mode": "phrase",
+    "answer": "mirror image",
+    "clue": "",
+    "image": "15.jpg"
+  },
+  {
+    "id": 16,
+    "mode": "phrase",
+    "answer": "bask in the sun",
+    "clue": "",
+    "image": "16.jpg"
+  },
+  {
+    "id": 17,
+    "mode": "phrase",
+    "answer": "split personality",
+    "clue": "",
+    "image": "17.jpg"
+  },
+  {
+    "id": 18,
+    "mode": "phrase",
+    "answer": "take time to chill",
+    "clue": "",
+    "image": "18.jpg"
+  },
+  {
+    "id": 19,
+    "mode": "phrase",
+    "answer": "two's a company",
+    "clue": "",
+    "image": "19.jpg"
+  },
+  {
+    "id": 20,
+    "mode": "phrase",
+    "answer": "picture perfect",
+    "clue": "",
+    "image": "20.jpg"
+  },
+  {
+    "id": 21,
+    "mode": "",
+    "answer": "",
+    "clue": "",
+    "image": ""
+  }
+];

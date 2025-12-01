fd98c7165bddd4b908df70e85a5f7b357304a832
fckaw <feichin.kaw@gmail.com>
2025-12-01 21:40:01 +0000

    Add files via upload

--- puzzles.js
+++ puzzles.js
@@ -17,7 +17,7 @@ window.puzzles = [
   {
     "id": 3,
     "mode": "phrase",
-    "answer": "nine lives",
+    "answer": "three's a crowd",
     "clue": "Three cats",
     "image": "3.jpg"
   },
@@ -146,5 +146,138 @@ window.puzzles = [
     "answer": "upward spiral",
     "clue": "",
     "image": "21.jpg"
+  },
+  {
+    "id": 22,
+    "mode": "phrase",
+    "answer": "bridge the gap",
+    "clue": "",
+    "image": "22.jpg"
+  },
+  {
+    "id": 23,
+    "mode": "phrase",
+    "answer": "divine spirit",
+    "clue": "",
+    "image": "23.jpg"
+  },
+  {
+    "id": 24,
+    "mode": "phrase",
+    "answer": "birds of a feather flock together",
+    "clue": "",
+    "image": "24.jpg"
+  },
+  {
+    "id": 25,
+    "mode": "phrase",
+    "answer": "spread your wings",
+    "clue": "",
+    "image": "25.jpg"
+  },
+  {
+    "id": 26,
+    "mode": "phrase",
+    "answer": "reach for the light",
+    "clue": "",
+    "image": "26.jpg"
+  },
+  {
+    "id": 27,
+    "mode": "phrase",
+    "answer": "greatest of all time",
+    "clue": "",
+    "image": "27.jpg"
+  },
+  {
+    "id": 28,
+    "mode": "phrase",
+    "answer": "camouflaged",
+    "clue": "",
+    "image": "28.jpg"
+  },
+  {
+    "id": 29,
+    "mode": "phrase",
+    "answer": "leave me alone",
+    "clue": "",
+    "image": "29.jpg"
+  },
+  {
+    "id": 30,
+    "mode": "phrase",
+    "answer": "cat nap",
+    "clue": "",
+    "image": "30.jpg"
+  },
+  {
+    "id": 31,
+    "mode": "phrase",
+    "answer": "don't be a yawn",
+    "clue": "",
+    "image": "31.jpg"
+  },
+  {
+    "id": 32,
+    "mode": "phrase",
+    "answer": "in deep contemplation",
+    "clue": "",
+    "image": "32.jpg"
+  },
+  {
+    "id": 33,
+    "mode": "phrase",
+    "answer": "cat got your tongue?",
+    "clue": "",
+    "image": "33.jpg"
+  },
+  {
+    "id": 34,
+    "mode": "phrase",
+    "answer": "crack a smile",
+    "clue": "",
+    "image": "34.jpg"
+  },
+  {
+    "id": 35,
+    "mode": "phrase",
+    "answer": "frog face",
+    "clue": "",
+    "image": "35.jpg"
+  },
+  {
+    "id": 36,
+    "mode": "phrase",
+    "answer": "easy on the eyes",
+    "clue": "",
+    "image": "36.jpg"
+  },
+  {
+    "id": 37,
+    "mode": "phrase",
+    "answer": "triple blessing",
+    "clue": "",
+    "image": "37.jpg"
+  },
+  {
+    "id": 38,
+    "mode": "phrase",
+    "answer": "from dawn to dusk",
+    "clue": "",
+    "image": "38.jpg"
+  },
+  {
+    "id": 39,
+    "mode": "phrase",
+    "answer": "up, up and away",
+    "clue": "",
+    "image": "39.jpg"
+  },
+  {
+    "id": 40,
+    "mode": "phrase",
+    "answer": "out of sorts",
+    "clue": "",
+    "image": "40.jpg"
   }
 ];
